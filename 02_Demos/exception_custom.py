76fa9eb3643b85f1bfa2e50265fc1c1b00fb5b51
Christophe D. <christophe.delory@labom2iformation.fr>
2025-09-30 15:48:39 +0200

    exception custom

--- 02_Demos/exception_custom.py
+++ 02_Demos/exception_custom.py
@@ -17,24 +17,61 @@ class AgeInvalideException(Exception):
 # raise AgeInvalideException("Age invalide")
 
 
-def input_age():
-    try:
-        age = int(input("Saisir votre Age : "))
+# def input_age():
+#     try:
+#         age = int(input("Saisir votre Age : "))
+#         if age<= 0 or age >=120:
+#             raise AgeInvalideException("Age invalide")
+#     except ValueError as ve:
+#         print(ve)
+#         print("Saisie invalide !")
+#         return -1
+#     except AgeInvalideException as aie: 
+#         print(aie)
+#         return -1
+#     else:
+#         print("Age valide !")
+#         return age
+
+
+# if __name__ == '__main__':
+#     age = -1
+#     while age == -1:
+#         age = input_age()
+
+# def input_age():
+#     age_local = int(input("Saisir votre Age : "))
+#     if age_local<= 0 or age_local >=120:
+#         raise AgeInvalideException("Age invalide")
+#     return age_local
+
+# if __name__ == "__main__":
+#     age = -1
+#     while age == -1:
+#         try:
+#             age = input_age()
+#         # except ValueError as ve:
+#         #     print(ve)
+#         #     print("Saisie invalide !")
+#         # except AgeInvalideException as aie:
+#         #     print (aie)
+#         except Exception as ex: # Attrape tout les types d'exception
+#             print(ex)
+#             print(type(ex))
+#         else:
+#             print("Age valide !")
+
+
+class Personne:
+    def __init__(self, age) -> None:
         if age<= 0 or age >=120:
             raise AgeInvalideException("Age invalide")
-    except ValueError as ve:
-        print(ve)
-        print("Saisie invalide !")
-        return -1
-    except AgeInvalideException as aie: 
-        print(aie)
-        return -1
-    else:
-        print("Age valide !")
-        return age
+        self.age = age
 
+try:
+    p = Personne(3000)
+except Exception:
+    print('Un problème est arrivé lors de la création de mon instance de personne')
 
-if __name__ == '__main__':
-    age = -1
-    while age == -1:
-        age = input_age()
+print('test')
+print(p) 
